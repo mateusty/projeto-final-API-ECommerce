31fe55221ffac9d0c14b8f2e78ae8500df1b96e7
Mateus Tamaki <mateustamaki43@gmail.com>
2026-05-22 18:32:47 -0300

    Adicionando entidades Cliente e Endereco

--- src/main/java/org/serratec/Ecommerce/entity/Cliente.java
+++ src/main/java/org/serratec/Ecommerce/entity/Cliente.java
@@ -1,10 +1,12 @@
 package org.serratec.Ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +14,27 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "cliente")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name="cliente_endereco",
+            joinColumns = @JoinColumn(name="id_cliente"),
+            inverseJoinColumns = @JoinColumn(name="id_endereco")
+    )
+    private List<Endereco> enderecos;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column
+    private String telefone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
 }
