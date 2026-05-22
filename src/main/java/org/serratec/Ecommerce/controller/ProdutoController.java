68d4ab0526adb2fcbb78b28819586037f6e7e2c9
Mateus Tamaki <mateustamaki43@gmail.com>
2026-05-22 18:11:39 -0300

    Alterando nome de controller

--- src/main/java/org/serratec/Ecommerce/controller/ProdutoController.java
+++ src/main/java/org/serratec/Ecommerce/controller/ProdutoController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/produtos")
-public class Produto {
+public class ProdutoController {
 
     private final ProdutoService produtoService;
 
-    public Produto(ProdutoService produtoService) {
+    public ProdutoController(ProdutoService produtoService) {
         this.produtoService = produtoService;
     }
 }
