8b1d1faab899bcbfd3b726cab642195299ffd400
caiolukas-residencia <caio.rocha@residente.serratec.org>
2026-05-26 18:35:36 -0300

    Criação das entities Pedido, Produto, bem como a tabela auxiliar ItemPedido e o ENUM StatusPedido

--- src/main/java/org/serratec/Ecommerce/entity/ItemPedido.java
+++ src/main/java/org/serratec/Ecommerce/entity/ItemPedido.java
@@ -4,31 +4,35 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.serratec.Ecommerce.enums.StatusPedido;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "pedido")
-public class Pedido {
-    
+@Table(name = "item_pedido")
+public class ItemPedido {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false)
-    private LocalDate data;
+    private int quantidade;
 
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private StatusPedido status;
+    private double valorVenda;
+
+    @Column(nullable = false)
+    private double desconto;
 
     @ManyToOne
-    @JoinColumn(name = "cliente_id")
-    private Cliente cliente;
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
 
 }
