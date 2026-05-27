d05762444a72531fd28f43702a2e991ac308a59e
caiolukas-residencia <caio.rocha@residente.serratec.org>
2026-05-26 21:37:51 -0300

    Implementação do PedidoController

--- src/main/java/org/serratec/Ecommerce/controller/PedidoController.java
+++ src/main/java/org/serratec/Ecommerce/controller/PedidoController.java
@@ -1,8 +1,16 @@
 package org.serratec.Ecommerce.controller;
 
+import jakarta.validation.Valid;
+import org.serratec.Ecommerce.entity.Pedido;
+import org.serratec.Ecommerce.model.PedidoRequest;
+import org.serratec.Ecommerce.model.PedidoResponse;
+import org.serratec.Ecommerce.model.PedidoUpdateRequest;
 import org.serratec.Ecommerce.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/pedidos")
@@ -12,5 +20,25 @@ public class PedidoController {
 
     public PedidoController(PedidoService pedidoService) {
         this.pedidoService = pedidoService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<PedidoResponse>> listarPedidos() {
+        return ResponseEntity.ok(pedidoService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoResponse> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(pedidoService.buscarPedidoPorId(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<PedidoResponse> inserirPedido (@Valid @RequestBody PedidoRequest pedido) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoService.inserirPedido(pedido));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PedidoResponse> atualizarPedido(@Valid @RequestBody PedidoUpdateRequest pedido, @PathVariable Long id) {
+        return ResponseEntity.ok(pedidoService.atualizarPedido(id, pedido));
     }
 }
