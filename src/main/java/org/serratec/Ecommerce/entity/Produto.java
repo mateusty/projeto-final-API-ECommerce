851746bc046a372d7f5b6589c25f6a7850b3a12b
caiolukas-residencia <caio.rocha@residente.serratec.org>
2026-05-27 01:13:26 -0300

    Sucesso no teste; recomentando classes do Produto para facilitar o merge

--- src/main/java/org/serratec/Ecommerce/entity/Produto.java
+++ src/main/java/org/serratec/Ecommerce/entity/Produto.java
@@ -1,27 +1,17 @@
-package org.serratec.Ecommerce.entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "produto")
-public class Produto {
-    // Placeholder para evitar erros na branch
-    // Substituir posteriormente pela implementação da Lilliane e Pedro
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    @Column(nullable = false)
-    private Double preco;
-
-    @Column(nullable = false)
-    private String nome;
-}
+//package org.serratec.Ecommerce.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.UUID;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Table(name = "produto")
+//public class Produto {
+//
+//}
