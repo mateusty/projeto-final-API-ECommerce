f109c1cbc821fabf6b875ca834c207b4fba32ba9
pedromartins01 <pedropintomartinsdesouza@gmail.com>
2026-05-28 16:50:59 -0300

    Acrescentando informações nos DTOs

--- src/main/java/org/serratec/Ecommerce/model/ProdutoUpdateRequest.java
+++ src/main/java/org/serratec/Ecommerce/model/ProdutoUpdateRequest.java
@@ -1,6 +1,7 @@
 package org.serratec.Ecommerce.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,16 +14,17 @@ import java.util.UUID;
 
 public class ProdutoUpdateRequest {
 
-    @Schema(nullable = true, description = "FK da categoria dentro do ProdutoUpdateRequest para atualizar")
+    @Schema(nullable = true, description = "FK da categoria dentro do ProdutoUpdateRequest para atualizar", example = "aea55b15-802c-4f3d-a88f-9fb11fb36d86")
     private UUID categoriaId;
 
-    @Schema(nullable = true, description = "FK da subcategoria dentro do ProdutoUpdateRequest para atualizar")
+    @Schema(nullable = true, description = "FK da subcategoria dentro do ProdutoUpdateRequest para atualizar", example = "aea55b15-802c-4f3d-a88f-9fb11fb36d86")
     private UUID subcategoriaId;
 
     @Schema(nullable = true, description = "Nome do produto a ser atualizado")
     private String nomeProduto;
 
     @Schema(nullable = true, description = "Valor do produto a ser atualizado")
+    @Positive(message = "O preço deve ser maior que zero.")
     private Double preco;
 
 }
